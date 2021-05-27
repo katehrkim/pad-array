d672dff8acb2d01ea6601f8e2b2c9a384d777548
Kate Kim <katehrkim@gmail.com>
2021-05-26 22:56:46 -0400

    Kate's answer

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,8 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    if len(array) < min_size:
+        count = len(array)
+        while count < min_size:
+            array.append(value)
+            count = count + 1
+    return array
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,13 @@
 # Write unit tests!
+import unittest
+from pad_array import pad
+
+class PadArrayTestCase(unittest.TestCase):
+    """Tests for pad_array.py"""
+    def test_one(self):
+        self.assertEqual(pad([1,2,3], 5), [1,2,3,None,None])
+    def test_two(self):
+        self.assertEqual(pad([1,2,3], 5, 'apple'), [1,2,3,'apple', 'apple'])
+
+if __name__ == '__main__':
+    unittest.main()
